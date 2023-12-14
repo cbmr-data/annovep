3f57098081f149a35375523e42dda608bb63a915
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2023-12-14 13:04:35 +0100

    add nox and pyproject.toml

---
+++ noxfile.py
@@ -0,0 +1,41 @@
+import nox
+
+nox.options.sessions = [
+    "style",
+    "lints",
+    "typing",
+]
+
+
+SOURCES = (
+    "annovep",
+    "bin/annovep",
+    "noxfile.py",
+    # "scripts",
+)
+
+
+RUFF_REQUIREMENT = "ruff==0.1.8"
+
+
+@nox.session
+def style(session: nox.Session) -> None:
+    session.install(RUFF_REQUIREMENT)
+    # Replaces `black --check`
+    session.run("ruff", "format", "--check", *SOURCES)
+    # Replaces `isort --check-only`
+    session.run("ruff", "check", "--select", "I", *SOURCES)
+
+
+@nox.session
+def lints(session: nox.Session) -> None:
+    session.install(RUFF_REQUIREMENT)
+    session.run("ruff", "check", *SOURCES)
+
+
+@nox.session()
+def typing(session: nox.Session) -> None:
+    session.install(".")
+    session.install("nox~=2023.4.22")
+    session.install("pyright==1.1.340")
+    session.run("pyright", *SOURCES)
